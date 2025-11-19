70bffaf2ea63a4b87aead6dc1486467dc56446ee
Anish-Programmer <at7anish@gmail.com>
2025-11-19 12:06:10 +0545

    Task 2 completed

---
+++ Task2/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Calculator
+    {
+        // PrintWelcome() method
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+        }
+
+        // Add() method
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+        // Multiply() method
+        public int Multiply(int num1, int num2=1)
+        {
+            return num1 * num2; ;
+        }
+
+
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // creating object of Calculatore class
+            Calculator calculator1 = new Calculator();
+
+            calculator1.PrintWelcome();
+            int sum = calculator1.Add(5,5);
+            int product = calculator1.Multiply(4); // using default value for num2
+
+            // Displaying results
+            Console.WriteLine("Sum: "+ sum);
+            Console.WriteLine("Product: "+ product);
         }
     }
 }
