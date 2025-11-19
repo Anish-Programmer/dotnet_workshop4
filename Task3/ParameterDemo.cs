6b505583fbd0853a331e42cc4415c36469ba2333
Anish-Programmer <at7anish@gmail.com>
2025-11-19 15:39:53 +0545

    Task 3 completed

---
+++ Task3/ParameterDemo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class ParameterDemo
+    {
+       // Increase(ref int number) method
+       public void Increase(ref int number)
+       {
+            number += 10; // increase the number by 10
+       }
+
+        // GetFullName(out string fullname) method
+        public void GetFullName(out string fullName)
+        {
+            fullName = "Anish Tamang"; // assignig the full name to output parameter
+        }
+
+        // SumAll(params int[] numbers) method
+        public int SumAll(params int[] numbers)
+        {
+            // defining sum variable
+            int sum = 0;
+
+            // using foreach loop to iterate
+            foreach (var num in numbers)
+            {
+                sum += num;  // adding each number to sum
+            }
+
+            return sum;
+        }
+
+    }
+}
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // creating an object of ParameterDemo class
+            ParameterDemo obj1 = new ParameterDemo();
+
+            // calling methods in ParameterDemo class
+            int x = 5;  // initializing parameter before calling ref method
+            Console.WriteLine($"Original value of x: {x} ");
+            obj1.Increase(ref x);  // callling ref method Increase()
+            Console.WriteLine($"Increased value of x by 10: {x} ");
+
+            
+            string fullName; // declaring out parameter
+            obj1.GetFullName(out fullName);  // calling out method GetFullName()
+            Console.WriteLine("Full Name: " + fullName);
+
+            
+            int result = obj1.SumAll(10,20,30,40,50); // passing multiple arguments to params method SumAll()
+            Console.WriteLine("Sum of all numbers: " + result);
         }
     }
 }
