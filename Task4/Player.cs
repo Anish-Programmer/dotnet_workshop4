27e6a291cf984d923bf8e1bbeb30b97e9653a596
Anish-Programmer <at7anish@gmail.com>
2025-11-19 16:09:20 +0545

    Task 4 completed

---
+++ Task4/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Player
+    {
+        // creating instance fields
+        public string? playerName;
+        public int level;
+        public string? health;
+
+
+        // creating default constructor
+        public Player()
+        { 
+            Console.WriteLine("Default Constructor has been called");
+        }
+
+        // creating parameterized constructor
+        public Player(string playerName, int level, string health)
+        { 
+            this.playerName = playerName;
+            this.level = level;
+            this.health = health;
+        }
+    }
+}
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // creating object of Player class using default constructor
+            Player player1 = new Player();
+            player1.playerName = "Shiv";
+            player1.level = 99;
+            player1.health = "Fit";
+
+            // printing the default values of default constructor
+            Console.WriteLine("\nPlayer1 details: (default constructor)");
+            Console.WriteLine($"Player Name: {player1.playerName}\nLevel: {player1.level}\nHealth: {player1.health}");
+          
+            // creating object of Player class using parameterized constructor
+            Player player2 = new Player("Ram",99,"Good");
+
+            // printing the values of player2 object
+            Console.WriteLine("\nPlayer2 details: (parameterized constructor)");
+            Console.WriteLine($"Player Name: {player2.playerName}\nLevel: {player2.level}\nHealth: {player2.health}");
         }
     }
 }
